54489e20d0cde1e13acbfc7cbdbfdaf1189f76f0
Marnus-hue <56795147+Marnus-hue@users.noreply.github.com>
2021-02-25 14:51:08 +0200

    Update Client.java
    Main class
    // Kyk net ek die layout van die switch gedoen en die scanner 
    // Ek 4 cases want ek dink die booking moet ook n class op sy eie wees

--- Documents/BC 3rd Year/PRG381/assessments/Vos_Dieudonne_Badenhorst_Marthinus_PRG381_Project1/Client.java
+++ Documents/BC 3rd Year/PRG381/assessments/Vos_Dieudonne_Badenhorst_Marthinus_PRG381_Project1/Client.java
@@ -1,5 +1,26 @@
 public class Client {
     public static void main(String[] args) {
-        //
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("\n1 Events \n2 Food \n3 Theme \n4 Booking \n0 Exit");  // Kyk net ek die layout van die switch gedoen en die scanner 
+        int option = sc.nextInt();                                                  // Ek 4 cases want ek dink die booking moet ook n class op sy eie wees 
+        
+        switch(option)
+        {
+            case 0 : 
+                Break;
+                
+            case 1 :  
+                Break;
+                
+            case 2 :  
+                Break;    
+                
+            case 3 :  
+                Break;   
+                
+            case 4 :  
+                Break;
+        }
     }    
 }
