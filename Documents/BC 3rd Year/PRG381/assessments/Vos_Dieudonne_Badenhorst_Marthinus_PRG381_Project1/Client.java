c928bbd288d3186590ad08e11e619886f4a52ece
Marnus-hue <56795147+Marnus-hue@users.noreply.github.com>
2021-02-26 12:28:49 +0200

    Update Client.java

--- Documents/BC 3rd Year/PRG381/assessments/Vos_Dieudonne_Badenhorst_Marthinus_PRG381_Project1/Client.java
+++ Documents/BC 3rd Year/PRG381/assessments/Vos_Dieudonne_Badenhorst_Marthinus_PRG381_Project1/Client.java
@@ -2,8 +2,8 @@ public class Client {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("\n1 Events \n2 Food \n3 Theme \n4 Booking \n0 Exit");  // Kyk net ek die layout van die switch gedoen en die scanner 
-        int option = sc.nextInt();                                                  // Ek 4 cases want ek dink die booking moet ook n class op sy eie wees 
+        System.out.print("\n1 Events \n2 Food \n3 Theme \n4 Booking \n5Manager \n0 Exit");  // Switch statement for the user to choose what they want.
+        int option = sc.nextInt();                                                   
         
         switch(option)
         {
